a7b2b074ec63aa82b2bb6491041245d81170d1b4
ranrmak <69759599+ranrmak@users.noreply.github.com>
2025-03-26 17:22:11 -0700

    Update handler.py

--- handler.py
+++ handler.py
@@ -20,3 +20,6 @@ def handler(event, context):
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
         raise e
+
+def getSecrets():
+    return "helloworld"
